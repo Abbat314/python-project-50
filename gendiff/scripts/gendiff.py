bb14bf06ff627927a038293e47b4ea8d5d76f5df
Andrey Belokon <student_mifp87@mail.ru>
2023-09-12 17:47:40 +0300

    the third step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse
+from gendiff.generate_diff import generate_diff
+
 
 def main():
     parser = argparse.ArgumentParser(
-                    prog='gendiff',
-                    description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', nargs=1)
-    parser.add_argument('second_file', nargs=1)
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.',
+        )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-
     args = parser.parse_args()
+    file1 = args.first_file
+    file2 = args.second_file
+    print(generate_diff(file1, file2))
+    
 
 if __name__ == '__main__':
     main()
